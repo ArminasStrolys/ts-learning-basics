7b10936e29342f72ecc7d9e8aabc7102d044578e
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-25 23:54:57 +0300

    Types - number type

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log('Hello, this is a test');
+var testNum = function (x, y) {
+    return x + y;
+};
+var num1 = 5;
+var num2 = 10.5;
+var res = testNum(num1, num2);
+console.log(res);
--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
 console.log('Hello, this is a test');
+let testNum = (x: number, y: number) => {
+  return x + y;
+};
+let num1 = -5;
+let num2 = 10.5;
+
+const result = testNum(num1, num2);
+console.log(result);
