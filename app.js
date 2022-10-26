5dbd7b1de68dc029efb20fa2f42984958334cfc0
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-26 21:20:48 +0300

    More types

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-console.log('Hello, this is a test');
 var testNum = function (x, y, showResult, phrase) {
     var result = x + y;
     showResult && console.log(phrase + result);
@@ -10,3 +9,42 @@ var printResult = true;
 var resultPhrase = 'Result is: ';
 var result = testNum(num1, num2, printResult, resultPhrase);
 console.log(result);
+//------------------------------------------------------------------------
+// const person: object = { // error, need specification
+// const ADMIN = 0
+// const READ_ONLY = 1
+// const AUTHOR = 2
+// Instead of ^
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 2] = "ADMIN";
+    Role[Role["READ_ONLY"] = 3] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 4] = "AUTHOR";
+})(Role || (Role = {})); // Enum labels
+var person = {
+    name: 'Jack',
+    age: 99,
+    hobbies: ['sports', 'cars'],
+    cars: ['bmw', 3],
+    role: Role.ADMIN
+};
+console.log(person.name);
+//------------------------------------------------------------------------
+var combine = function (inp1, inp2, resultConversion) {
+    var result;
+    if (typeof inp1 === 'number' &&
+        typeof inp2 === 'number' &&
+        resultConversion === 'as-number') {
+        result = +inp1 + +inp2; // +inp1 converts value to a number
+    }
+    else {
+        result = inp1.toString() + ' ' + inp2.toString();
+    }
+    return result;
+};
+var test1 = combine(50, 40, 'as-number');
+console.log(test1);
+var test2 = combine('Marry', 'Joe', 'as-string');
+console.log(test2);
+var test3 = combine(66, 99, 'as-string');
+console.log(test3);
--- app.ts
+++ app.ts
@@ -14,16 +14,58 @@ console.log(result);
 //------------------------------------------------------------------------
 
 // const person: object = { // error, need specification
+
+// const ADMIN = 0
+// const READ_ONLY = 1
+// const AUTHOR = 2
+// Instead of ^
+
+enum Role {
+  ADMIN = 2,
+  READ_ONLY,
+  AUTHOR,
+} // Enum labels
+
 const person: {
   name: string;
   age: number;
   hobbies: string[];
   cars: [string, number];
+  role: number;
 } = {
   name: 'Jack',
   age: 99,
   hobbies: ['sports', 'cars'],
   cars: ['bmw', 3],
+  role: Role.ADMIN,
 };
 
 console.log(person.name);
+
+//------------------------------------------------------------------------
+
+const combine = (
+  inp1: number | string,
+  inp2: number | string,
+  // resultConversion: string
+  resultConversion: 'as-number' | 'as-string' // a better way
+) => {
+  let result;
+  if (
+    typeof inp1 === 'number' &&
+    typeof inp2 === 'number' &&
+    resultConversion === 'as-number'
+  ) {
+    result = +inp1 + +inp2; // +inp1 converts value to a number
+  } else {
+    result = inp1.toString() + ' ' + inp2.toString();
+  }
+  return result;
+};
+
+const test1 = combine(50, 40, 'as-number');
+console.log(test1);
+const test2 = combine('Marry', 'Joe', 'as-string');
+console.log(test2);
+const test3 = combine(66, 99, 'as-string');
+console.log(test3);
