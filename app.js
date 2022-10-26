1b0906b3611cd0e082224ec41d3c28130780a06a
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-26 19:39:46 +0300

    Some examples for TS

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 console.log('Hello, this is a test');
-var testNum = function (x, y) {
-    return x + y;
+var testNum = function (x, y, showResult, phrase) {
+    var result = x + y;
+    showResult && console.log(phrase + result);
+    return result;
 };
-var num1 = 5;
+var num1 = -5;
 var num2 = 10.5;
-var res = testNum(num1, num2);
-console.log(res);
+var printResult = true;
+var resultPhrase = 'Result is: ';
+var result = testNum(num1, num2, printResult, resultPhrase);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
-console.log('Hello, this is a test');
-let testNum = (x: number, y: number) => {
-  return x + y;
+let testNum = (x: number, y: number, showResult: boolean, phrase: string) => {
+  const result = x + y;
+  showResult && console.log(phrase + result);
+  return result;
 };
-let num1 = -5;
-let num2 = 10.5;
+const num1 = -5;
+const num2 = 10.5;
+const printResult = true;
+const resultPhrase = 'Result is: ';
 
-const result = testNum(num1, num2);
+const result = testNum(num1, num2, printResult, resultPhrase);
 console.log(result);
