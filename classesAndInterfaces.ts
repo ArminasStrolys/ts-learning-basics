3b9b8ff0cd4fd48a7cb7e93da1b9fd04e57a87f0
Arminas Strolys <arminas.strolys@gmail.com>
2022-10-28 18:15:05 +0300

    Classes and instances TS

---
+++ classesAndInterfaces.ts
@@ -0,0 +1,12 @@
+console.log('test');
+
+class Person {
+  name: string;
+  constructor(t: string) {
+    this.name = t;
+  }
+}
+
+const humanity = new Person('Roger');
+
+console.log(humanity);
---
+++ src/classesAndInterfaces.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log('test');
+class Person {
+    constructor(t) {
+        this.name = t;
+    }
+}
+const humanity = new Person('Roger');
+console.log(humanity);
