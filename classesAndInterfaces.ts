07f9da356890123a1388288b301d04f310e5dc7f
Arminas Strolys <arminas.strolys@gmail.com>
2022-10-28 22:10:23 +0300

    Interfaces TS

--- classesAndInterfaces.ts
+++ classesAndInterfaces.ts
@@ -18,3 +18,36 @@ humanity.descrption();
 const humanityCopy = { name: 'Elly', descrption: humanity.descrption };
 
 humanityCopy.descrption();
+
+//-----------------------
+
+interface Persona {
+  name: string;
+  age: number;
+
+  greet(greeting: string): void;
+}
+
+let humanoid1: Persona;
+
+humanoid1 = {
+  name: 'Aegon',
+  age: 15,
+  greet(greeting: string) {
+    console.log(greeting + ' ' + this.name);
+  },
+};
+
+humanoid1.greet('Hi, I"m');
+
+interface AddFn {
+  // function interface
+  (a: number, b: number): number;
+}
+
+// type AddFunc = (c: number, d: number) => number // alternative
+
+interface OptionalArgs {
+  name: string;
+  surname?: string; // ? means optional argument
+}
--- src/classesAndInterfaces.js
+++ src/classesAndInterfaces.js
@@ -15,3 +15,12 @@ console.log(humanity);
 humanity.descrption();
 const humanityCopy = { name: 'Elly', descrption: humanity.descrption };
 humanityCopy.descrption();
+let humanoid1;
+humanoid1 = {
+    name: 'Aegon',
+    age: 15,
+    greet(greeting) {
+        console.log(greeting + ' ' + this.name);
+    },
+};
+humanoid1.greet('Hi, I"m');
