f722a110a411182c6c67f80a73d53eaf73783e86
Arminas Strolys <arminas.strolys@gmail.com>
2022-10-28 18:54:29 +0300

    .

--- classesAndInterfaces.ts
+++ classesAndInterfaces.ts
@@ -1,12 +1,20 @@
 console.log('test');
 
 class Person {
-  name: string;
-  constructor(t: string) {
-    this.name = t;
+  // name: string;
+  constructor(public name: string) {
+    // this.name = t; // shortcut ^
+  }
+  descrption(this: Person) {
+    console.log('This guy: ' + this.name);
   }
 }
 
 const humanity = new Person('Roger');
 
 console.log(humanity);
+humanity.descrption();
+
+const humanityCopy = { name: 'Elly', descrption: humanity.descrption };
+
+humanityCopy.descrption();
--- src/classesAndInterfaces.js
+++ src/classesAndInterfaces.js
@@ -1,9 +1,17 @@
 "use strict";
 console.log('test');
 class Person {
-    constructor(t) {
-        this.name = t;
+    // name: string;
+    constructor(name) {
+        this.name = name;
+        // this.name = t; // shortcut ^
+    }
+    descrption() {
+        console.log('This guy: ' + this.name);
     }
 }
 const humanity = new Person('Roger');
 console.log(humanity);
+humanity.descrption();
+const humanityCopy = { name: 'Elly', descrption: humanity.descrption };
+humanityCopy.descrption();
