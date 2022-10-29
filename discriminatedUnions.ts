43c712747e2be4ed50849c09a53e6a96180b6ec4
Arminas Strolys <arminas.strolys@gmail.com>
2022-10-29 21:31:44 +0300

    Discriminated unions

---
+++ discriminatedUnions.ts
@@ -0,0 +1,24 @@
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+const move = (animal: Animal) => {
+  let speed;
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSpeed;
+  }
+  console.log('Moving at speed: ' + speed);
+};
+
+move({ type: 'bird', flyingSpeed: 15 });
