678e78465443c0245835b2895dad33afe43f784b
Arminas Strolys <arminas.strolys@gmail.com>
2022-10-30 22:08:13 +0200

    Update generics.ts

--- generics.ts
+++ generics.ts
@@ -1,2 +1,11 @@
 // const names = ['Mac', 'Sarah']; SAME AS BELOW
 const names: Array<string> = [];
+
+//---------------
+
+function merge<T extends object, U>(objC: T, objB: U) {
+  return Object.assign(objC, objB);
+}
+
+const mergeObj = merge({ name: 'Stewart' }, { age: 66 });
+console.log(mergeObj.age);
