8651e5155d58bf93cda84267b6170b86a33ee84a
Arminas Strolys <arminas.strolys@gmail.com>
2022-10-30 18:20:03 +0200

    Update typeCasting.ts

--- typeCasting.ts
+++ typeCasting.ts
@@ -1,1 +1,3 @@
-const inputEl = document.getElementById('user-input') as HTMLInputElement; // THIS IS TYPE CASTINGS
+const inputEl = document.getElementById('user-input') as HTMLInputElement; // THIS IS TYPE CASTING
+// OR LIKE THIS
+// const addFunc  = (5, 10) as number;
